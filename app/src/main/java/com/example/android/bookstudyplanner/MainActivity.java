9b171a241cb983ce7e3e7c73de325924752a694e
vanessa <vanessa.guillemain@gmail.com>
2019-07-08 22:09:00 +0200

    feat : First 3 tabs skeletons

--- app/src/main/java/com/example/android/bookstudyplanner/MainActivity.java
+++ app/src/main/java/com/example/android/bookstudyplanner/MainActivity.java
@@ -1,13 +1,28 @@
 package com.example.android.bookstudyplanner;
 
+import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
+import android.support.design.widget.TabLayout;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
+
+    private TabAdapter adapter;
+    private TabLayout tabLayout;
+    private ViewPager viewPager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        viewPager = (ViewPager) findViewById(R.id.viewPager);
+        tabLayout = (TabLayout) findViewById(R.id.tabLayout);
+        adapter = new TabAdapter(getSupportFragmentManager());
+        adapter.addFragment(new TabBooksFragment(), "Books");
+        adapter.addFragment(new TabTodayFragment(), "Today");
+        adapter.addFragment(new TabPlanningFragment(), "Planning");
+        viewPager.setAdapter(adapter);
+        tabLayout.setupWithViewPager(viewPager);
     }
 }
