3267884c349ebb6e52048c2abb02073fdc7d1515
vanessa <vanessa.guillemain@gmail.com>
2019-07-09 09:34:49 +0200

    feat : Add an AppBar in MainActivity

--- app/src/main/java/com/example/android/bookstudyplanner/MainActivity.java
+++ app/src/main/java/com/example/android/bookstudyplanner/MainActivity.java
@@ -1,9 +1,13 @@
 package com.example.android.bookstudyplanner;
 
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.support.design.widget.TabLayout;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -24,5 +28,17 @@ public class MainActivity extends AppCompatActivity {
         adapter.addFragment(new TabPlanningFragment(), "Planning");
         viewPager.setAdapter(adapter);
         tabLayout.setupWithViewPager(viewPager);
+
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
     }
 }
