3f408a19bd6c50d88fdd222505f234ee0c2e4a88
vanessa <vanessa.guillemain@gmail.com>
2019-07-12 18:13:38 +0200

    feat : Added fields in DataBase. Change BookDetail layout to Constraint layout

--- app/src/main/java/com/example/android/bookstudyplanner/database/AppDatabase.java
+++ app/src/main/java/com/example/android/bookstudyplanner/database/AppDatabase.java
@@ -1,9 +1,11 @@
 package com.example.android.bookstudyplanner.database;
 
+import android.arch.persistence.db.SupportSQLiteDatabase;
 import android.arch.persistence.room.Database;
 import android.arch.persistence.room.Room;
 import android.arch.persistence.room.RoomDatabase;
 import android.arch.persistence.room.TypeConverters;
+import android.arch.persistence.room.migration.Migration;
 import android.content.Context;
 import android.util.Log;
 
@@ -11,7 +13,7 @@ import android.util.Log;
  * Created by vanessa on 10/07/2019.
  */
 
-@Database(entities = {BookEntity.class}, version = 1, exportSchema = false)
+@Database(entities = {BookEntity.class}, version = 2, exportSchema = false)
 @TypeConverters(DateConverter.class)
 public abstract class AppDatabase extends RoomDatabase {
 
@@ -26,6 +28,7 @@ public abstract class AppDatabase extends RoomDatabase {
                 Log.d(LOG_TAG, "Creating new database instance");
                 sInstance = Room.databaseBuilder(context.getApplicationContext(),
                         AppDatabase.class, AppDatabase.DATABASE_NAME)
+                        .addMigrations(MIGRATION_1_2)
                         .build();
             }
         }
@@ -34,5 +37,20 @@ public abstract class AppDatabase extends RoomDatabase {
     }
 
     public abstract BookDao bookDao();
+
+    static final Migration MIGRATION_1_2 = new Migration(1, 2) {
+        @Override
+        public void migrate(SupportSQLiteDatabase database) {
+            database.execSQL("ALTER TABLE book ADD COLUMN fromPageNb INTEGER");
+            database.execSQL("ALTER TABLE book ADD COLUMN toPageNb INTEGER");
+            database.execSQL("ALTER TABLE book ADD COLUMN nbPagesToRead INTEGER");
+            database.execSQL("ALTER TABLE book ADD COLUMN beginDate INTEGER");
+            database.execSQL("ALTER TABLE book ADD COLUMN endDate INTEGER");
+            database.execSQL("ALTER TABLE book ADD COLUMN weekPlanning TEXT");
+            database.execSQL("ALTER TABLE book ADD COLUMN nbPagesRead INTEGER");
+            database.execSQL("ALTER TABLE book ADD COLUMN readTimeInSeconds INTEGER");
+            database.execSQL("ALTER TABLE book ADD COLUMN nbSecondsByPage INTEGER");
+        }
+    };
 
 }
--- app/src/main/java/com/example/android/bookstudyplanner/database/BookEntity.java
+++ app/src/main/java/com/example/android/bookstudyplanner/database/BookEntity.java
@@ -1,9 +1,12 @@
 package com.example.android.bookstudyplanner.database;
 
 import android.arch.persistence.room.Entity;
+import android.arch.persistence.room.Ignore;
 import android.arch.persistence.room.PrimaryKey;
 import android.os.Parcel;
 import android.os.Parcelable;
+
+import java.util.Date;
 
 /**
  * Created by vanessa on 10/07/2019.
@@ -15,7 +18,52 @@ public class BookEntity implements Parcelable   {
     private int isbn;
     private String title;
     private int pageCount;
+    private Integer fromPageNb;
+    private Integer toPageNb;
+    private Integer nbPagesToRead;
+    private Date beginDate;
+    private Date endDate;
+    private String weekPlanning;
+    private Integer nbPagesRead;
+    private Integer readTimeInSeconds;
+    private Integer nbSecondsByPage;
 
+    @Ignore
+    private float percentRead;
+
+    @Ignore
+    public BookEntity(int id, int isbn, String title, int pageCount, Integer fromPageNb, Integer toPageNb, Integer nbPagesToRead, Date beginDate, Date endDate, String weekPlanning, Integer nbPagesRead, Integer readTimeInSeconds, Integer nbSecondsByPage) {
+        this.id = id;
+        this.isbn = isbn;
+        this.title = title;
+        this.pageCount = pageCount;
+        this.fromPageNb = fromPageNb;
+        this.toPageNb = toPageNb;
+        this.nbPagesToRead = nbPagesToRead;
+        this.beginDate = beginDate;
+        this.endDate = endDate;
+        this.weekPlanning = weekPlanning;
+        this.nbPagesRead = nbPagesRead;
+        this.readTimeInSeconds = readTimeInSeconds;
+        this.nbSecondsByPage = nbSecondsByPage;
+    }
+
+    public BookEntity(int isbn, String title, int pageCount, Integer fromPageNb, Integer toPageNb, Integer nbPagesToRead, Date beginDate, Date endDate, String weekPlanning, Integer nbPagesRead, Integer readTimeInSeconds, Integer nbSecondsByPage) {
+        this.isbn = isbn;
+        this.title = title;
+        this.pageCount = pageCount;
+        this.fromPageNb = fromPageNb;
+        this.toPageNb = toPageNb;
+        this.nbPagesToRead = nbPagesToRead;
+        this.beginDate = beginDate;
+        this.endDate = endDate;
+        this.weekPlanning = weekPlanning;
+        this.nbPagesRead = nbPagesRead;
+        this.readTimeInSeconds = readTimeInSeconds;
+        this.nbSecondsByPage = nbSecondsByPage;
+    }
+
+    @Ignore
     public BookEntity(int isbn, String title, int pageCount) {
         this.isbn = isbn;
         this.title = title;
@@ -28,6 +76,25 @@ public class BookEntity implements Parcelable   {
         this.isbn = parcel.readInt();
         this.title = parcel.readString();
         this.pageCount = parcel.readInt();
+        this.fromPageNb = (Integer)parcel.readSerializable();
+        this.toPageNb = (Integer)parcel.readSerializable();
+        this.nbPagesToRead = (Integer)parcel.readSerializable();
+        Long begin = (Long) parcel.readSerializable();
+        if (begin == null) {
+            this.beginDate = null;
+        } else {
+            this.beginDate = new Date(begin);
+        }
+        Long end = (Long) parcel.readSerializable();
+        if (end == null) {
+            this.endDate = null;
+        } else {
+            this.endDate = new Date(end);
+        }
+        this.weekPlanning = (String)parcel.readSerializable();
+        this.nbPagesRead = (Integer)parcel.readSerializable();
+        this.readTimeInSeconds = (Integer)parcel.readSerializable();
+        this.nbSecondsByPage = (Integer)parcel.readSerializable();
     }
 
     @Override
@@ -41,6 +108,23 @@ public class BookEntity implements Parcelable   {
         dest.writeInt(isbn);
         dest.writeString(title);
         dest.writeInt(pageCount);
+        dest.writeSerializable(fromPageNb);
+        dest.writeSerializable(toPageNb);
+        dest.writeSerializable(nbPagesToRead);
+        if(beginDate != null) {
+            dest.writeSerializable(beginDate.getTime());
+        } else {
+            dest.writeSerializable(null);
+        }
+        if(endDate != null) {
+            dest.writeSerializable(endDate.getTime());
+        } else {
+            dest.writeSerializable(null);
+        }
+        dest.writeSerializable(weekPlanning);
+        dest.writeSerializable(nbPagesRead);
+        dest.writeSerializable(readTimeInSeconds);
+        dest.writeSerializable(nbSecondsByPage);
     }
 
     public static final Parcelable.Creator CREATOR = new Parcelable.Creator<BookEntity>() {
@@ -86,6 +170,86 @@ public class BookEntity implements Parcelable   {
 
     public void setPageCount(int pageCount) {
         this.pageCount = pageCount;
+    }
+
+    public Integer getFromPageNb() {
+        return fromPageNb;
+    }
+
+    public void setFromPageNb(Integer fromPageNb) {
+        this.fromPageNb = fromPageNb;
+    }
+
+    public Integer getToPageNb() {
+        return toPageNb;
+    }
+
+    public void setToPageNb(Integer toPageNb) {
+        this.toPageNb = toPageNb;
+    }
+
+    public Integer getNbPagesToRead() {
+        return nbPagesToRead;
+    }
+
+    public void setNbPagesToRead(Integer nbPagesToRead) {
+        this.nbPagesToRead = nbPagesToRead;
+    }
+
+    public Date getBeginDate() {
+        return beginDate;
+    }
+
+    public void setBeginDate(Date beginDate) {
+        this.beginDate = beginDate;
+    }
+
+    public Date getEndDate() {
+        return endDate;
+    }
+
+    public void setEndDate(Date endDate) {
+        this.endDate = endDate;
+    }
+
+    public String getWeekPlanning() {
+        return weekPlanning;
+    }
+
+    public void setWeekPlanning(String weekPlanning) {
+        this.weekPlanning = weekPlanning;
+    }
+
+    public Integer getNbPagesRead() {
+        return nbPagesRead;
+    }
+
+    public void setNbPagesRead(Integer nbPagesRead) {
+        this.nbPagesRead = nbPagesRead;
+    }
+
+    public Integer getReadTimeInSeconds() {
+        return readTimeInSeconds;
+    }
+
+    public void setReadTimeInSeconds(Integer readTimeInSeconds) {
+        this.readTimeInSeconds = readTimeInSeconds;
+    }
+
+    public Integer getNbSecondsByPage() {
+        return nbSecondsByPage;
+    }
+
+    public void setNbSecondsByPage(Integer nbSecondsByPage) {
+        this.nbSecondsByPage = nbSecondsByPage;
+    }
+
+    public float getPercentRead() {
+        if(nbPagesToRead>0) {
+            return nbPagesRead / nbPagesToRead * 100;
+        } else {
+            return 0;
+        }
     }
 
     @Override
