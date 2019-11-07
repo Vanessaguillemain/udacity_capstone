68b0bdfa6374ecee7925766b522e3bfa31deb2a9
vanessa <vanessa.guillemain@gmail.com>
2019-11-07 18:59:10 +0100

    Fit : BookDetail Screen is more adaptable

--- app/src/main/java/com/example/android/bookstudyplanner/Utils.java
+++ app/src/main/java/com/example/android/bookstudyplanner/Utils.java
@@ -1,11 +1,15 @@
 package com.example.android.bookstudyplanner;
 
+import android.app.Activity;
 import android.content.Context;
 import android.os.Build;
+import android.util.DisplayMetrics;
 import android.util.Log;
+import android.view.Display;
 import android.widget.DatePicker;
 import android.widget.Toast;
 
+import java.lang.reflect.Method;
 import java.text.DateFormat;
 import java.text.ParseException;
 import java.util.ArrayList;
@@ -406,4 +410,47 @@ public class Utils {
         }
         return 0;
     }
+
+    public static void getScreenSize(Context context, Activity activity) {
+        Display display = activity.getWindowManager().getDefaultDisplay();
+        int realWidth;
+        int realHeight;
+
+        if (Build.VERSION.SDK_INT >= 17){
+            //new pleasant way to get real metrics
+            DisplayMetrics realMetrics = new DisplayMetrics();
+            display.getRealMetrics(realMetrics);
+            realWidth = realMetrics.widthPixels;
+            realHeight = realMetrics.heightPixels;
+
+        } else if (Build.VERSION.SDK_INT >= 14) {
+            //reflection for this weird in-between time
+            try {
+                Method mGetRawH = Display.class.getMethod("getRawHeight");
+                Method mGetRawW = Display.class.getMethod("getRawWidth");
+                realWidth = (Integer) mGetRawW.invoke(display);
+                realHeight = (Integer) mGetRawH.invoke(display);
+            } catch (Exception e) {
+                //this may not be 100% accurate, but it's all we've got
+                realWidth = display.getWidth();
+                realHeight = display.getHeight();
+                Log.e("Display Info", "Couldn't use reflection to get the real display metrics.");
+            }
+
+        } else {
+            //This should be close, as lower API devices should not have window navigation bars
+            realWidth = display.getWidth();
+            realHeight = display.getHeight();
+        }
+
+        Log.d("Display Info", "realWidth=" + realWidth);
+        Log.d("Display Info", "realHeight=" + realHeight);
+
+        DisplayMetrics displayMetrics = context.getResources().getDisplayMetrics();
+        float dpHeight = displayMetrics.heightPixels / displayMetrics.density;
+        float dpWidth = displayMetrics.widthPixels / displayMetrics.density;
+        Log.d("Display Info", "dpWidth=" + dpWidth);
+        Log.d("Display Info", "dpHeight=" + dpHeight);
+    }
+
 }
